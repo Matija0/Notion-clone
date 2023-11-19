2f0c205bf1842652cc8de7f8813b394e4656eea0
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-11-19 18:58:30 +0100

    Text editor, publish note, error screen

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -6,13 +6,16 @@ import { useUser } from "@clerk/clerk-react"
 import { useMutation } from "convex/react"
 import { PlusCircle } from "lucide-react"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 
 const Documents = () => {
+  const router= useRouter()
   const {user}=useUser()
   const create= useMutation(api.documents.create)
   const onCreate= () =>{
     const promise= create({title: "New Note"})
+    .then((documentId)=> router.push(`/documents/${documentId}`))
     toast.promise(promise, {
       loading: "Creating note...",
       success: "Note created!",
